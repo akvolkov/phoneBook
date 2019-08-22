7c04f89026ab9eabe4e7896a7bb9e096c3ec768b
Aleksey Volkov <aleksey_volkov88@mail.ru>
2019-08-23 01:58:55 +0300

    tests

--- src/test/java/com/my/company/AppTest.java
+++ src/test/java/com/my/company/AppTest.java
@@ -1,24 +1,28 @@
 package com.my.company;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
- * Unit test for simple App.
+ * Unit test for App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+    private static Map<String, List<String>> phoneBook = new HashMap<>();
+    static {
+        phoneBook.put("Иванов И.И.", Arrays.asList("+8 800 2000 500", "+8 800 200 600"));
+        phoneBook.put("Петров П.П.", Arrays.asList("+8 800 2000 700"));
+        phoneBook.put("Сидоров С.С.", Arrays.asList("+8 800 2000 800", "+8 800 2000 900", "+8 800 2000 000"));
     }
 
+    // Тут нечего тестировать
+    // оО что тут можно оттестировать? оО
     @Test
     public void printPhones() {
+        final App app = new App(phoneBook);
+        app.printPhones("Иванов И.И.");
     }
 }
